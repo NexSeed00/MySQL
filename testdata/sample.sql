ca976df12d7bf76f7d257cdbbb1930ec2182cf9f
Atsushi <atsushi88885@gmail.com>
2020-02-19 09:22:45 +0800

    joinのサンプル追加

--- testdata/sample.sql
+++ testdata/sample.sql
@@ -21,3 +21,22 @@ UPDATE tweets SET body = '更新テスト' WHERE id = 5;
 -- delete
 -- tweetsテーブルからidが7のレコードを削除する
 DELETE FROM tweets WHERE id = 7; 
+
+
+-- JOIN(INNNER JOIN)
+-- 2つのテーブルを結合する。
+-- tasksテーブルのuser_idとusersテーブルのidが一致するレコードを合体させる
+-- 両方のテーブルに存在するレコードだけ取得
+-- (tasksテーブルにuser_idが1のレコードがあっても、usersテーブルにidが1のレコードがない場合、出力されない)
+SELECT * FROM tasks JOIN users ON tasks.user_id = users.id
+
+
+-- JOIN(OUTER JOIN)
+-- 基本はINNER JOINと一緒
+-- 条件に一致するメインテーブル(FROMの後に書いてあるテーブル)のレコードは全て出力する
+-- (tasksテーブルにuser_idが1のレコードがあれば、usersテーブルにidが1のレコードがない場合でも出力される)
+SELECT * FROM tasks LEFT JOIN users ON tasks.user_id = users.id
+
+-- 通常のSELECT文同様WHEREで条件の指定も可能
+-- どのテーブルのカラムかわかるようにテーブル名.idという形で指定する
+SELECT * FROM tasks LEFT JOIN users ON tasks.user_id = users.id WHERE users.id = 1
